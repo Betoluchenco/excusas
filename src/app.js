559d0281319e213020e673a89564201a017b1bd2
Betoluchenco <63628143+Betoluchenco@users.noreply.github.com>
2025-04-21 12:28:13 +0000

    proyecto finalizado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,16 @@
 import "bootstrap";
 import "./style.css";
-
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#random-message").innerText = [
+    "A two headed pinecone yelled at my sandwich in my house",
+    "A cat just hacked my WiFi",
+    "I saw a fish driving a car",
+    "An avocado tried to fight me",
+    "A ghost danced in my mirror",
+    "A pigeon just ordered pizza from my phone"
+  ][Math.floor(Math.random() * 6)];
 };
+
